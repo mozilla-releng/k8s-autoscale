7a4d23ef99b0db889ec9eceda6b98f177b157334
Rail Aliiev <rail@mozilla.com>
2020-01-17 09:21:07 -0500

    Log Sentry init

--- src/k8s_autoscale/logging.py
+++ src/k8s_autoscale/logging.py
@@ -10,6 +10,8 @@ import sentry_sdk
 from dockerflow.logging import JsonLogFormatter
 from sentry_sdk.integrations.logging import LoggingIntegration
 
+logger = logging.getLogger(__name__)
+
 
 def configure_logging(level):
     handler = logging.StreamHandler(sys.stdout)
@@ -19,4 +21,5 @@ def configure_logging(level):
 
 
 def configure_sentry(environment, sentry_dsn):
+    logger.info("Enabling Sentry for %s", environment)
     sentry_sdk.init(dsn=sentry_dsn, environment=environment, integrations=[LoggingIntegration()])
