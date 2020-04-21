7688bcd87f1995cfd2b3eade7359cb2c7833831d
Rail Aliiev <rail@mozilla.com>
2020-04-21 11:27:32 -0400

    Add SLA vs task duration validation

--- src/k8s_autoscale/validate.py
+++ src/k8s_autoscale/validate.py
@@ -6,3 +6,17 @@ def validate(config, schema):
     config = yaml.safe_load(config)
     schema = yaml.safe_load(schema)
     jsonschema.validate(config, schema)
+    verify_sla_task_duration(config)
+
+
+def verify_sla_task_duration(config):
+    def verify_sla(worker_type):
+        if (
+            worker_type["autoscale"]["args"]["sla_seconds"]
+            < worker_type["autoscale"]["args"]["avg_task_duration"]
+        ):
+            return f"{worker_type['worker_type']}: sla_seconds shouldn't be less than avg_task_duration"
+
+    errors = list(filter(None, [verify_sla(worker_type) for worker_type in config["worker_types"]]))
+    if errors:
+        raise ValueError("\n".join(errors))
