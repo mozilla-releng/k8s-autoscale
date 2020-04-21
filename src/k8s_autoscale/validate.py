2df812410fcec56d3386300ad03168fbc2a457f0
Rail Aliiev <rail@mozilla.com>
2020-04-21 14:17:43 -0400

    Renames

--- src/k8s_autoscale/validate.py
+++ src/k8s_autoscale/validate.py
@@ -6,17 +6,17 @@ def validate(config, schema):
     config = yaml.safe_load(config)
     schema = yaml.safe_load(schema)
     jsonschema.validate(config, schema)
-    verify_sla_task_duration(config)
+    verify_slo_task_duration(config)
 
 
-def verify_sla_task_duration(config):
-    def verify_sla(worker_type):
+def verify_slo_task_duration(config):
+    def verify_slo(worker_type):
         if (
-            worker_type["autoscale"]["args"]["sla_seconds"]
+            worker_type["autoscale"]["args"]["slo_seconds"]
             < worker_type["autoscale"]["args"]["avg_task_duration"]
         ):
-            return f"{worker_type['worker_type']}: sla_seconds shouldn't be less than avg_task_duration"
+            return f"{worker_type['worker_type']}: slo_seconds shouldn't be less than avg_task_duration"
 
-    errors = list(filter(None, [verify_sla(worker_type) for worker_type in config["worker_types"]]))
+    errors = list(filter(None, [verify_slo(worker_type) for worker_type in config["worker_types"]]))
     if errors:
         raise ValueError("\n".join(errors))
