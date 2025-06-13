674ee35c13ac3c5e068fb04314ce6c0be10b6fff
Bastien Orivel <borivel@mozilla.com>
2025-06-13 15:23:40 +0200

    Remove version.txt
    This is not needed anymore since we switched to uv. I forgot to remove
    it.

--- taskcluster/docker/skopeo/push_image.sh
+++ taskcluster/docker/skopeo/push_image.sh
@@ -30,6 +30,10 @@ EOF
 
 umoci insert --image k8s_autoscale:final version.json /app/version.json
 
+APP_VERSION="$(cat /pyproject.toml | sed -n 's/version = "\(.*\)"/\1/p')"
+DOCKER_TAG="$(echo ${DOCKER_TAG} | cut -f3 -d/)"
+DOCKER_ARCHIVE_TAG="${DOCKER_TAG}-${APP_VERSION}-$(date +%Y%m%d%H%M%S)-${VCS_HEAD_REV}"
+
 if [ $DRYRUN = 1 ];
 then
     echo "Skipping push because DRYRUN is 1"
@@ -40,9 +44,6 @@ else
     chmod 600 $HOME/.docker/config.json
 
     echo "=== Pushing to docker hub ==="
-    APP_VERSION="$(cat /version.txt)"
-    DOCKER_TAG="$(echo ${DOCKER_TAG} | cut -f3 -d/)"
-    DOCKER_ARCHIVE_TAG="${DOCKER_TAG}-${APP_VERSION}-$(date +%Y%m%d%H%M%S)-${VCS_HEAD_REV}"
     skopeo copy oci:k8s_autoscale:final docker://$DOCKER_REPO:$DOCKER_TAG
     skopeo inspect docker://$DOCKER_REPO:$DOCKER_TAG
     skopeo copy oci:k8s_autoscale:final docker://$DOCKER_REPO:$DOCKER_ARCHIVE_TAG
